9558441d73a90df280fcb0832d716d88b2a23393
Husejn <husejn138@outlook.com>
2024-01-09 12:05:30 -0500

    Updating change

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,31 +1,29 @@
-from django.urls import path, include
+from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
-from django.contrib import admin
 
 app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
     # name the URL
-    path(route='', view=views.index, name='index'),
-    #path(route='admin/', admin.site.urls),
-    #path(route='djangoapp/', include('djangoapp.urls')),
+
     # path for about view
-    path(route='about/', view=views.about, name='about'),
+    path(route='about', view=views.about, name='about'),
     # path for contact us view
-    path(route='contactus/', view=views.contact, name='contact'),
+    path(route='contact', view=views.contact, name='contact'),
     # path for registration
-
+    path(route='register', view=views.registration_request, name='register'),
     # path for login
-
+    path(route='login', view=views.login_request, name='login'),
     # path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
-
+    path(route='home', view=views.get_dealerships, name='home'),
     # path for add a review view
-
+    path(route='add-review/<int:dealer_id>/<str:dealer_name>/', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
